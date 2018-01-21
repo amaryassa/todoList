84b6fe2422ee86a98c782921c951fa5acda3cabf
Amar Yassa <amaryassa@yahoo.fr>
2018-01-21 17:44:33 +0100

    Creation d'un nouveau composant Task, et comme il n'a pas de State ( c'est un composant Stateless) donc on le transforme en const

--- src/index.js
+++ src/index.js
@@ -3,44 +3,69 @@ import { render } from 'react-dom';
 
 
 class Tasklist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            tasks : [
+                {
+                    id: 1,
+                    description: 'Se lever',
+                    complete: true
+
+                },
+                {
+                    id: 2,
+                    description: 'réviser',
+                    complete: true
+
+                },
+                {
+                    id: 3,
+                    description: 'Dormir',
+                    complete: false
+
+                }
+            ]
+        };
+
+    }
     
     render() {
-        const tasks= [
-            {
-                id: 1,
-                description: 'Se lever',
-                complete: true
-    
-            },
-            {
-                id: 2,
-                description: 'réviser',
-                complete: true
-    
-            },
-            {
-                id: 3,
-                description: 'Dormir',
-                complete: false
-    
-            }
-        ];
         return (
             
             <div>
                 <h1> All Task</h1>
-                {tasks.map(task =>{
+                {this.state.tasks.map(task =>{
                     return (
-                        <article>
-                            <h1>#{task.id} - {task.description}</h1>
-                        </article>
+                        // <Task taskDetail={task}  />
+                        <Task key={task.id} task={task} />
                     )
-                })
-            }
+                })}
             </div>
         )
     }
 }
+
+const Task= (props)=>(
+    <article>
+        <h1>#{props.task.id} - {props.task.description} {props.task.complete ? '✔' : '❌'}</h1>
+    </article>
+
+)
+
+// class Task extends React.Component {
+//     render() {
+//         return (
+//             <article>
+//                 <h1>#{this.props.task.id} - {this.props.task.description} {this.props.task.complete ? '✔' : '❌'}
+//                 </h1>
+//             </article>
+         
+//         )
+//     }
+// }
+
+
 
 render(
     <Tasklist />,
