c58d8c9e6512fa01ef4349d5067aa09452f577ea
Amar Yassa <amaryassa@yahoo.fr>
2018-01-21 17:10:03 +0100

    Récupération de la liste des taches dans un array et un map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-// Code React.js
+import React from 'react';
+import { render } from 'react-dom';
+
+
+class Tasklist extends React.Component {
+    
+    render() {
+        const tasks= [
+            {
+                id: 1,
+                description: 'Se lever',
+                complete: true
+    
+            },
+            {
+                id: 2,
+                description: 'réviser',
+                complete: true
+    
+            },
+            {
+                id: 3,
+                description: 'Dormir',
+                complete: false
+    
+            }
+        ];
+        return (
+            
+            <div>
+                <h1> All Task</h1>
+                {tasks.map(task =>{
+                    return (
+                        <article>
+                            <h1>#{task.id} - {task.description}</h1>
+                        </article>
+                    )
+                })
+            }
+            </div>
+        )
+    }
+}
+
+render(
+    <Tasklist />,
+    document.getElementById('root')
+);
